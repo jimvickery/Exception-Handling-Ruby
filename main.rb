e3e4c0f323a32d1847e10f17012cc34d44a760bb
Jim Vickery <jimrv8@yahoo.com>
2017-10-16 11:35:21 -0500

    MVP

--- main.rb
+++ main.rb
@@ -1,11 +1,40 @@
 require_relative 'patient'
 
 new_patient = Patient.new("Jisie", "David")
-puts new_patient.first_name
-puts new_patient.last_name
-puts patient_contact_info
-puts new_patient.patient_status(:coma)
-puts new_patient.patient_currently_admitted?
-puts new_patient.patient_new_total(125)
-new_patient.add_med_to_list("lipitor", 123.12)
-new_patient.patient_medications_list
+
+    puts new_patient.first_name
+    puts new_patient.last_name
+
+
+
+begin puts patient_contact_info
+rescue NameError
+    puts "Patient contact info not complete (NameError)"
+end    
+
+
+begin puts puts new_patient.patient_status(:coma)
+rescue ArgumentError
+    puts "Arguemnt cannot divide by zero (ArgumentError)"
+end   
+
+begin puts new_patient.patient_currently_admitted?
+rescue ArgumentError
+    puts "Patient admitted status not complete"
+end   
+
+begin puts new_patient.patient_new_total(125)
+rescue ZeroDivisionError
+    puts "Patient contact info not complete (ZeroDivisionError)"
+end  
+
+
+begin new_patient.add_med_to_list("lipitor", 123.12)
+rescue ArgumentError
+    puts "Not an integer error (Argument (ArgumentError)"
+end
+
+begin new_patient.patient_medications_list
+rescue RuntimeError
+    puts "med list error (Runtime)"
+end
